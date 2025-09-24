11302095acb54a781754e8e8f9d445a1ed0110c8
mompei <ogunborjerry@gmail.com>
2025-09-24 18:17:21 +0100

    number_plate added to handler signature

--- programs/parkat-anchor/src/lib.rs
+++ programs/parkat-anchor/src/lib.rs
@@ -9,7 +9,7 @@ declare_id!("CJbYiHnNrzYe7imm54hYA9HiJS1Q8BJs5okxFJbhuUx3");
 pub mod parkat_anchor {
     use super::*;
 
-    pub fn init_user(ctx: Context<InitUser>) -> Result<()> {
+    pub fn init_user(ctx: Context<InitUser>, number_plate: String) -> Result<()> {
         ctx.accounts.init_user(&ctx.bumps, number_plate)
     }
 
