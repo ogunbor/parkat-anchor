f9035dcb27d9efffed0a60c7c9d795a88a594ac2
mompei <ogunborjerry@gmail.com>
2025-09-26 08:26:20 +0100

    process_exit changes

--- programs/parkat-anchor/src/instructions/process_exit.rs
+++ programs/parkat-anchor/src/instructions/process_exit.rs
@@ -32,49 +32,57 @@ impl<'info> ProcessExit<'info> {
     pub fn process_exit(&mut self) -> Result<()> {
         let car = &mut self.car;
 
-        // get current blockchain time
-        let current_time = Clock::get()?.unix_timestamp;
-
-        // calculate duration parked
-        let duration = current_time
-            .checked_sub(car.time_stamp)
-            .ok_or_else(|| error!(Error::TimeCalculationError))?;
-
-        let duration_u64 =
-            u64::try_from(duration).map_err(|_| error!(Error::TimeCalculationError))?;
-
-        // calculate amount to transfer
-        let amount = duration_u64
-            .checked_div(1000)
-            .ok_or_else(|| error!(Error::AmountCalculationError))?;
-
-        // check vault balance
-        let vault_balance = self.vault.to_account_info().lamports();
-        if amount > vault_balance {
-            return Err(error!(Error::InsufficientVaultBalance));
+        // Check if user is currently parked
+        if !car.is_parked {
+            return Err(error!(Error::NotCurrentlyParked));
         }
 
-        // perform CPI transfer
-        let cpi_program = self.system_program.to_account_info();
-        let cpi_accounts = Transfer {
-            from: self.vault.to_account_info(),
-            to: self.admin_wallet.to_account_info(),
-        };
-        let seeds = &[
-            b"vault",
-            car.to_account_info().key.as_ref(),
-            &[car.vault_bump],
-        ];
-        let signer_seeds = &[&seeds[..]];
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        // Get current blockchain time
+        let current_time = Clock::get()?.unix_timestamp;
 
-        transfer(cpi_ctx, amount)?;
+        // Calculate duration parked
+        let duration = current_time
+            .checked_sub(car.time_stamp)
+            .ok_or_else(|| error!(Error::InvalidParkingDuration))?;
 
-        // update on-chain state
-        car.amount = car
-            .amount
-            .checked_sub(amount)
-            .ok_or_else(|| error!(Error::AmountCalculationError))?;
+        // Convert to u64
+        let duration_u64 = u64::try_from(duration)
+            .map_err(|_| error!(Error::InvalidParkingDuration))?;
+
+        let amount = duration_u64 / 1000;
+
+        // Check if we need to transfer (only if amount > 0)
+        if amount > 0 {
+            // Check vault balance
+            let vault_balance = self.vault.to_account_info().lamports();
+            if amount > vault_balance {
+                return Err(error!(Error::InsufficientVaultBalance));
+            }
+
+            // Perform CPI transfer
+            let cpi_program = self.system_program.to_account_info();
+            let cpi_accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.admin_wallet.to_account_info(),
+            };
+            let seeds = &[
+                b"vault",
+                car.to_account_info().key.as_ref(),
+                &[car.vault_bump],
+            ];
+            let signer_seeds = &[&seeds[..]];
+            let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+            transfer(cpi_ctx, amount)?;
+
+            // Update car amount
+            car.amount = car
+                .amount
+                .checked_sub(amount)
+                .ok_or_else(|| error!(Error::AmountCalculationError))?;
+        }
+
+        // Update parking state
         car.time_stamp = current_time;
         car.is_parked = false;
 
@@ -87,7 +95,13 @@ pub enum Error {
     #[msg("Time calculation failed")]
     TimeCalculationError,
 
-    #[msg("Amount calculation failed")]
+    #[msg("Invalid parking duration - current time is before parking start time")]
+    InvalidParkingDuration,
+
+    #[msg("User is not currently parked")]
+    NotCurrentlyParked,
+
+    #[msg("Amount calculation failed - insufficient balance for parking fee")]
     AmountCalculationError,
 
     #[msg("Vault does not have enough balance")]
